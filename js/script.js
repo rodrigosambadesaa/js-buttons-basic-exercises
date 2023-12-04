9279c3a78d8c5cd194a4360d3f565eaa56d637dc
Rodrigo Sambade <rodrigosambadesaa@gmail.com>
2023-12-04 09:37:07 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+document // Page Visibility API
+  .addEventListener(
+    'visibilitychange',
+    function () {
+      const URL = document.hidden ? '../img/favicon.ico' : '../img/favicon-active.ico'
+      document.querySelector('[rel*="icon"]')
+          .setAttribute('href', URL)
+    }
+  )
+
 function miFuncion() {
     alert("Hola, Rodrigo")
 }
